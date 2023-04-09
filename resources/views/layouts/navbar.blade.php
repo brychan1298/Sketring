56e2b01c175a44fbe00c315695ef56e36f156360
brychan1298 <brychan58@gmail.com>
2023-04-09 14:28:54 +0700

    Add Tailwind

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css'])
+</head>
+<body>
+    <nav class="bg-[#850000] p-[20px]">
+        <h1 class="text-white">
+            Sketring
+        </h1>
+    </nav>
+</body>
+</html>
