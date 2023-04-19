763f7c927e27001d32cf81106743ef3acd09073d
brychan1298 <brychan58@gmail.com>
2023-04-19 22:38:56 +0700

    Perbaiki tombol edit profile

--- resources/views/konsumen/editProfile.blade.php
+++ resources/views/konsumen/editProfile.blade.php
@@ -168,8 +168,11 @@
                         <input type="text" name="Nohp" class="px-[24px] py-[16px] text-black bg-white rounded-[10px]"
                             value="{{ Auth::User()->Nohp }}">
                     </div>
-                    <div>
-                        <button type="submit" class="text-[#DC0000] bg-white px-7 py-2 text-2xl rounded-md my-5">
+                    <div class="flex gap-5">
+                        <a href="/profilekonsumen" class="font-bold border-2 text-2xl px-7 py-2 my-5 rounded-md hover:text-[#DC0000] hover:bg-white">
+                            BATAL
+                        </a>
+                        <button type="submit" class="border-2 text-[#DC0000] bg-white px-7 py-2 text-2xl rounded-md my-5 hover:bg-[#850000] hover:text-white">
                             SIMPAN
                         </button>
                     </div>
