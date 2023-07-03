ef4e7e1d6fe203c6fed99add96ba5b32ab89b6e0
brychan1298 <brychan58@gmail.com>
2023-07-03 15:07:25 +0700

    Remove Package

--- config/app.php
+++ config/app.php
@@ -195,7 +195,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Kreait\Laravel\Firebase\ServiceProvider::class,
     ],
 
     /*
